25846c7199cf6bfc1ac158987e21fdee40d926ae
Meghna Bakshi <meghnabakshi20@gmail.com>
2020-04-27 17:52:25 +0530

    my first commit

---
+++ tests/calculator.js
@@ -0,0 +1,30 @@
+let homepage = require('../pages/homepage')
+
+describe('semo calculator tests', function () {
+
+    it('addition test', function () {
+
+
+        homepage.get('http://juliemr.github.io/protractor-demo/');
+
+
+        homepage.enterFirstNumber('4');
+
+
+        homepage.enterSecondNumber('5');
+
+
+
+        homepage.clickGo();
+
+
+
+        homepage.verifyResult('9');
+
+        browser.sleep(2000);
+
+    });
+
+    
+
+});
